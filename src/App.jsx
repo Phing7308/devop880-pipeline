5d27bc36a36aa069a20b24ae2ca07782fa5473e8
Phing2310 <parichat.mang@ku.th>
2026-03-29 10:42:14 +0000

    Update App component content and logo image

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
+        <img src="03.jpg" className="App-logo" alt="logo" />
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+          CI/CD Pipeline for React Web app 
         </p>
         <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
+         Written by Mr.Parichat Mangkonkit
         </p>
         <p>
           <a
